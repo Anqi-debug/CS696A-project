66d13453f371a75f9d7c0950d536ea4398ff9dfd
Anqi <anqichen1028@gmail.com>
2024-11-11 22:34:40 -0500

    Edit connection with MongoDB

--- backend/server.js
+++ backend/server.js
@@ -15,8 +15,8 @@ mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 })
-.then(() => console.log("MongoDB connected"))
-.catch(err => console.log(err));
+.then(() => console.log("MongoDB Atlas connected"))
+.catch(err => console.log("Error: " + err));
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
